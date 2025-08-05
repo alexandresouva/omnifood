cd51fa0a5d8de1cb4d31dbfbb7edaf54bd562109
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-08-04 22:36:33 -0300

    chore: scaffold initial project structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Header, Footer],
+  template: `
+    <app-header />
+    <main></main>
+    <app-footer />
+  `
 })
-export class App {
-  protected readonly title = signal('omnifood');
-}
+export class App {}
